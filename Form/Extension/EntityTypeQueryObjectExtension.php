3a7dcf3e7c2346da7254dfdf4688684bf9692942
Pavel Batecko <pavel.batecko@imatic.cz>
2014-04-25 18:21:58 +0200

    RESEARCH-82 - Podpora query_object u form entity typu

---
+++ Form/Extension/EntityTypeQueryObjectExtension.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Imatic\Bundle\DataBundle\Form\Extension;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Imatic\Bundle\DataBundle\Data\Query\QueryObjectInterface;
+use Imatic\Bundle\DataBundle\Data\Query\QueryExecutorInterface;
+use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityLoaderInterface;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\AbstractTypeExtension;
+
+/**
+ * Form type theme extension
+ *
+ * @author Pavel Batecko <pavel.batecko@imatic.cz>
+ */
+class EntityTypeQueryObjectExtension extends AbstractTypeExtension
+{
+    /** @var QueryExecutorInterface */
+    private $queryExecutor;
+
+    public function __construct(QueryExecutorInterface $queryExecutor)
+    {
+        $this->queryExecutor = $queryExecutor;
+    }
+
+    /**
+     * @param EntityManagerInterface $em
+     * @param QueryObjectInterface   $queryObject
+     * @return EntityLoaderInterface
+     */
+    public function getLoader(EntityManagerInterface $em, QueryObjectInterface $queryObject)
+    {
+        return new EntityTypeQueryObjectLoader(
+            $em,
+            $this->queryExecutor,
+            $queryObject
+        );
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            'query_object' => null,
+            'loader' => function (Options $options, $previousLoader) {
+                if (null !== $options['query_object']) {
+                    if (null !== $options['query_builder']) {
+                        throw new \LogicException('Cannot use both "query_object" and "query_builder" options');
+                    }
+
+                    return $this->getLoader(
+                        $options['em'],
+                        $options['query_object']
+                    );
+                } else {
+                    return $previousLoader;
+                }
+            },
+        ]);
+
+        $resolver->setAllowedTypes([
+            'query_object' => ['null', 'Imatic\Bundle\DataBundle\Data\Query\QueryObjectInterface'],
+        ]);
+    }
+
+    public function getExtendedType()
+    {
+        return 'entity';
+    }
+}
---
+++ Form/Extension/EntityTypeQueryObjectLoader.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Imatic\Bundle\DataBundle\Form\Extension;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Imatic\Bundle\DataBundle\Data\Query\QueryExecutorInterface;
+use Imatic\Bundle\DataBundle\Data\Query\QueryObjectInterface;
+use Imatic\Bundle\DataBundle\Data\Driver\DoctrineORM\QueryObjectInterface as DoctrineORMQueryObjectInterface;
+use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityLoaderInterface;
+
+class EntityTypeQueryObjectLoader implements EntityLoaderInterface
+{
+    /** @var EntityManagerInterface */
+    private $em;
+    /** @var QueryExecutorInterface */
+    private $queryExecutor;
+    /** @var QueryObjectInterface */
+    private $queryObject;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        QueryExecutorInterface $queryExecutor,
+        QueryObjectInterface $queryObject
+    ) {
+        $this->em = $em;
+        $this->queryExecutor = $queryExecutor;
+        $this->queryObject = $queryObject;
+    }
+
+    public function getEntities()
+    {
+        return $this->queryExecutor->execute($this->queryObject);
+    }
+
+    public function getEntitiesByIds($identifier, array $values)
+    {
+        if (!$this->queryObject instanceof DoctrineORMQueryObjectInterface) {
+            throw new \RuntimeException('Unsupported query object');
+        }
+
+        $qb = $this->queryObject->build($this->em);
+        $alias = current($qb->getRootAliases());
+        $parameter = 'EntityTypeQueryObjectLoader_getEntitiesByIds_' . $identifier;
+        $where = $qb->expr()->in($alias . '.' . $identifier, ':' . $parameter);
+
+        // guess type
+        $entity = current($qb->getRootEntities());
+        $metadata = $this->em->getClassMetadata($entity);
+        if (in_array($metadata->getTypeOfField($identifier), array('integer', 'bigint', 'smallint'))) {
+            $parameterType = Connection::PARAM_INT_ARRAY;
+        } else {
+            $parameterType = Connection::PARAM_STR_ARRAY;
+        }
+
+        return $qb
+            ->andWhere($where)
+            ->getQuery()
+            ->setParameter($parameter, $values, $parameterType)
+            ->getResult()
+        ;
+    }
+}
