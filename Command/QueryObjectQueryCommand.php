4efa2dd137a84d4bdc2a023e51b5e950e3e44344
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2014-04-28 15:09:43 +0200

    RESEARCH-84: Possibility to pass arguments into query object using QueryObjectQueryCommand

--- Command/QueryObjectQueryCommand.php
+++ Command/QueryObjectQueryCommand.php
@@ -6,25 +6,37 @@ use Doctrine\Common\Util\Debug;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class QueryObjectQueryCommand extends ContainerAwareCommand
 {
+    const OPTION_ARGS = 'args';
+
     protected function configure()
     {
         $this
             ->setName('imatic:data:query-object-query')
             ->setDescription('Execute query defined in query object')
-            ->addArgument('class', InputArgument::REQUIRED, 'Query object class');
+            ->addArgument('class', InputArgument::REQUIRED, 'Query object class')
+            ->addOption(static::OPTION_ARGS, null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'List of arguments to pass into query object')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $queryExecutor = $this->getContainer()->get('imatic_data.query_executor');
         $class = $input->getArgument('class');
-        $queryObject = new $class;
+        $args = $input->getOption(static::OPTION_ARGS);
+
+        $classRef = new \ReflectionClass($class);
+        $queryObject = $classRef->newInstanceArgs($args);
+
+        $queryExecutor = $this->getContainer()->get('imatic_data.query_executor');
         $result = $queryExecutor->execute($queryObject);
 
+        ob_start();
         Debug::dump($result);
+        $output->writeln(ob_get_contents());
+        ob_clean();
     }
 }
---
+++ Tests/Unit/Command/QueryObjectQueryCommandTest.php
@@ -0,0 +1,80 @@
+<?php
+namespace Imatic\Bundle\DataBundle\Tests\Unit\Command;
+
+use Imatic\Bundle\DataBundle\Command\QueryObjectQueryCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class QueryObjectQueryCommandTest extends \PHPUnit_Framework_TestCase
+{
+    private $containerMock;
+
+    protected function setUp()
+    {
+        $this->containerMock = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+        $this->containerMock
+            ->expects($this->any())
+            ->method('get')
+            ->will($this->returnCallback(function ($id) {
+                switch ($id) {
+                    case 'imatic_data.query_executor':
+                        return $this->getMock('Imatic\Bundle\DataBundle\Data\Query\QueryExecutorInterface');
+                }
+            }))
+        ;
+    }
+
+    public function testCommandShouldShouldPrintResultOfQueryObject()
+    {
+        $queryObjectQueryCommand = new QueryObjectQueryCommand();
+        $queryObjectQueryCommand->setContainer($this->containerMock);
+        $application = new Application();
+        $application->add($queryObjectQueryCommand);
+
+        $command = $application->find('imatic:data:query-object-query');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+            'class' => 'Imatic\Bundle\DataBundle\Tests\Fixtures\TestProject\ImaticDataBundle\Query\UserListQuery',
+        ]);
+
+        $this->assertRegExp('/^NULL/', $commandTester->getDisplay());
+    }
+
+    public function testCommandShouldShouldPrintResultOfQueryObjectWithRequiredArguments()
+    {
+        $queryObjectQueryCommand = new QueryObjectQueryCommand();
+        $queryObjectQueryCommand->setContainer($this->containerMock);
+        $application = new Application();
+        $application->add($queryObjectQueryCommand);
+
+        $command = $application->find('imatic:data:query-object-query');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+            'class' => 'Imatic\Bundle\DataBundle\Tests\Fixtures\TestProject\ImaticDataBundle\Query\UserQuery',
+            '--args' => [1],
+        ]);
+
+        $this->assertRegExp('/^NULL/', $commandTester->getDisplay());
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testCommandShouldThrowExceptionIfArgumentIsNotPassedToQueryObjectWithRequiredArguments()
+    {
+        $queryObjectQueryCommand = new QueryObjectQueryCommand();
+        $queryObjectQueryCommand->setContainer($this->containerMock);
+        $application = new Application();
+        $application->add($queryObjectQueryCommand);
+
+        $command = $application->find('imatic:data:query-object-query');
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([
+            'command' => $command->getName(),
+            'class' => 'Imatic\Bundle\DataBundle\Tests\Fixtures\TestProject\ImaticDataBundle\Query\UserQuery',
+        ]);
+    }
+}
