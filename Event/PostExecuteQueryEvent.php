b73da4d7501e0c5054f5c30abc4d317458ac3ce9
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2014-09-18 14:46:05 +0200

    RESEARCH-106: Add query object into post_execute_query event

--- Event/PostExecuteQueryEvent.php
+++ Event/PostExecuteQueryEvent.php
@@ -2,16 +2,26 @@
 
 namespace Imatic\Bundle\DataBundle\Event;
 
+use Imatic\Bundle\DataBundle\Data\Query\QueryObjectInterface;
 use Symfony\Component\EventDispatcher\Event;
 
 class PostExecuteQueryEvent extends Event
 {
+    /** @var QueryObjectInterface */
+    protected $queryObject;
+
     /** @var mixed */
     protected $result;
 
-    public function __construct($result)
+    public function __construct(QueryObjectInterface $queryObject, $result)
     {
+        $this->queryObject = $queryObject;
         $this->result = $result;
+    }
+
+    public function getQueryObject()
+    {
+        return $this->queryObject;
     }
 
     public function getResult()
