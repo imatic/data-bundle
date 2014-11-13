944dee5dfcef481e1cad7951162f35c6f94f0550
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2014-11-13 11:35:51 +0100

    RESEARCH-114: DisplayCriteria ExtJs reader

--- Data/Query/DisplayCriteria/Reader/RequestQueryReader.php
+++ Data/Query/DisplayCriteria/Reader/RequestQueryReader.php
@@ -2,61 +2,18 @@
 
 namespace Imatic\Bundle\DataBundle\Data\Query\DisplayCriteria\Reader;
 
-use Symfony\Component\HttpFoundation\RequestStack;
-
 /**
  * @author Miloslav Nenadal <miloslav.nenadal@imatic.cz>
  */
-class RequestQueryReader implements DisplayCriteriaReader
+class RequestQueryReader extends SessionReader
 {
-    /**
-     * @var RequestStack
-     */
-    protected $requestStack;
-
-    public function __construct(RequestStack $requestStack)
-    {
-        $this->requestStack = $requestStack;
-    }
-
-    public function clearAttribute($name, $component = null, $emptyValue = null)
-    {
-        if (
-            ($session = ($this->requestStack->getCurrentRequest()->getSession()))
-            && ($sessionKey = $this->getAttributeSessionKey($name, $component))
-        ) {
-            if (null === $emptyValue) {
-                $session->remove($sessionKey);
-            } else {
-                $session->set($sessionKey, $emptyValue);
-            }
-        }
-    }
-
     public function readAttribute($name, $default = null, $component = null, $persistent = true)
     {
         $request = $this->requestStack->getCurrentRequest();
 
-        $path = $name;
-        if ($component) {
-            $path = $component . '[' . $name . ']';
-        }
-
-        $value = $request->query->get($path, null, true);
-
-        if (
-            $persistent
-            && ($session = $request->getSession())
-            && ($sessionKey = $this->getAttributeSessionKey($name, $component))
-        ) {
-            if (null === $value) {
-                if ($session->has($sessionKey)) {
-                    $value = $session->get($sessionKey);
-                }
-            } else {
-                $session->set($sessionKey, $value);
-            }
-        }
+        $path = $this->createAttributePath($name, $component);
+        $queryValue = $request->query->get($path, null, true);
+        $value = parent::readAttribute($name, $queryValue, $component, $persistent);
 
         return null !== $value
             ? $value
@@ -64,23 +21,12 @@ class RequestQueryReader implements DisplayCriteriaReader
         ;
     }
 
-    /**
-     * @param string $name
-     * @param string $component
-     *
-     * @return string|null
-     */
-    protected function getAttributeSessionKey($name, $component = null)
+    protected function createAttributePath($attributeName, $component = null)
     {
-        $request = $this->requestStack->getCurrentRequest();
-
-        if ($request->attributes->has('_route') && $request->attributes->has('_route_params')) {
-            return sprintf(
-                'imatic.data.display_criteria.%s(%s|%s)',
-                null !== $component ? "{$component}[{$name}]" : $name,
-                $request->attributes->get('_route'),
-                serialize($request->attributes->get('_route_params'))
-            );
+        if ($component) {
+            return $component . '[' . $attributeName . ']';
         }
+
+        return $attributeName;
     }
 }
---
+++ Tests/Unit/Data/Query/DisplayCriteria/Reader/ExtJsReaderTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Imatic\Bundle\DataBundle\Tests\Unit\Data\Query\DisplayCriteria\Reader;
+
+use Imatic\Bundle\DataBundle\Data\Query\DisplayCriteria\FilterOperatorMap;
+use Imatic\Bundle\DataBundle\Data\Query\DisplayCriteria\Reader\ExtJsReader;
+use Imatic\Bundle\DataBundle\Data\Query\DisplayCriteria\SorterRule;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @author Miloslav Nenadal <miloslav.nenadal@imatic.cz>
+ */
+class ExtJsReaderTest extends \PHPUnit_Framework_TestCase
+{
+    protected $requestStack;
+    protected $currentRequest;
+
+    protected function setUp()
+    {
+
+        $requestStack = $this->getMock('Symfony\Component\HttpFoundation\RequestStack');
+
+        $requestStack
+            ->expects($this->any())
+            ->method('getCurrentRequest')
+            ->will($this->returnCallback(function () {
+                return $this->currentRequest;
+            }))
+        ;
+
+        $this->requestStack = $requestStack;
+    }
+
+    public function testReadingAttributesFromRequest()
+    {
+        $this->currentRequest = new Request([
+            'componentFromRequest' => [
+                'filter' => json_encode([
+                    [
+                        'property' => 'name',
+                        'value' => 'Lee',
+                    ],
+                ]),
+                'sort' => json_encode([
+                    [
+                        'property' => 'name',
+                        'direction' => 'ASC',
+                    ],
+                ]),
+                'page' => 31,
+                'limit' => 123,
+            ],
+        ]);
+
+        $reader = new ExtJsReader($this->requestStack);
+
+        $filter = $reader->readAttribute('filter', null, 'componentFromRequest');
+        $this->assertEquals([
+            'name' => [
+                'value' => 'Lee',
+                'operator' => FilterOperatorMap::OPERATOR_EQUAL,
+            ],
+        ], $filter);
+
+        $sorter = $reader->readAttribute('sorter', null, 'componentFromRequest');
+        $this->assertEquals([
+            'name' => SorterRule::ASC,
+        ], $sorter);
+
+        $this->assertEquals(31, $reader->readAttribute('page', null, 'componentFromRequest'));
+        $this->assertEquals(123, $reader->readAttribute('limit', null, 'componentFromRequest'));
+    }
+
+    public function testReadingAttributesFromRequestWithSimpleSorter()
+    {
+        $this->currentRequest = new Request([
+            'componentFromRequest' => [
+                'filter' => json_encode([
+                    [
+                        'property' => 'name',
+                        'value' => 'Lee',
+                    ],
+                ]),
+                'sort' => 'name',
+                'dir' => 'ASC',
+                'page' => 31,
+                'limit' => 123,
+            ],
+        ]);
+
+        $reader = new ExtJsReader($this->requestStack);
+
+        $filter = $reader->readAttribute('filter', null, 'componentFromRequest');
+        $this->assertEquals([
+            'name' => [
+                'value' => 'Lee',
+                'operator' => FilterOperatorMap::OPERATOR_EQUAL,
+            ],
+        ], $filter);
+
+        $sorter = $reader->readAttribute('sorter', null, 'componentFromRequest');
+        $this->assertEquals([
+            'name' => SorterRule::ASC,
+        ], $sorter);
+
+        $this->assertEquals(31, $reader->readAttribute('page', null, 'componentFromRequest'));
+        $this->assertEquals(123, $reader->readAttribute('limit', null, 'componentFromRequest'));
+    }
+}
