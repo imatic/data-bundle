bbfa278ec80f8ade87762961773ff18ff03a6c4d
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-01-08 18:26:08 +0100

    RESEARCH-111: Pridani array display critera factory, ktere se pouziva u nekolika projektu

---
+++ Data/Query/DisplayCriteria/ArrayDisplayCriteriaFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Imatic\Bundle\DataBundle\Data\Query\DisplayCriteria;
+
+use Imatic\Bundle\DataBundle\Data\Query\DisplayCriteria\Reader\ArrayReader;
+use Symfony\Component\Form\FormFactoryInterface;
+
+/**
+ * @author Miloslav Nenadal <miloslav.nenadal@imatic.cz>
+ */
+class ArrayDisplayCriteriaFactory extends DisplayCriteriaFactory
+{
+    public function __construct(PagerFactory $pagerFactory, FormFactoryInterface $formFactory)
+    {
+        parent::__construct($pagerFactory, $formFactory, new ArrayReader());
+    }
+
+    public function setAttributes(array $attributes = [])
+    {
+        $this->displayCriteriaReader = new ArrayReader($attributes);
+    }
+}
