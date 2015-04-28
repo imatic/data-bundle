aa60bc16885dd9c8cfc639d02c6867cedea0fc29
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-04-28 15:33:03 +0200

    RESEARCH-130: Podpora vice databazovych pripojeni

---
+++ Data/Query/QueryExecutorDelegate.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Imatic\Bundle\DataBundle\Data\Query;
+
+use Imatic\Bundle\DataBundle\Data\Query\DisplayCriteria\DisplayCriteriaInterface;
+
+class QueryExecutorDelegate implements QueryExecutorInterface
+{
+    /** @var QueryExecutorFactoryInterface */
+    private $queryExecutorFactory;
+
+    public function __construct(QueryExecutorFactoryInterface $queryExecutorFactory)
+    {
+        $this->queryExecutorFactory = $queryExecutorFactory;
+    }
+
+    public function count(QueryObjectInterface $queryObject, DisplayCriteriaInterface $displayCriteria = null)
+    {
+        return $this->getQueryExecutor($queryObject)->count($queryObject, $displayCriteria);
+    }
+
+    public function execute(QueryObjectInterface $queryObject, DisplayCriteriaInterface $displayCriteria = null)
+    {
+        return $this->getQueryExecutor($queryObject)->execute($queryObject, $displayCriteria);
+    }
+
+    /**
+     * @param QueryObjectInterface $queryObject
+     *
+     * @return QueryExecutorInterface
+     */
+    protected function getQueryExecutor(QueryObjectInterface $queryObject)
+    {
+        $connection = 'default';
+        if ($queryObject instanceof ConnectionQueryObjectInterface) {
+            $connection = $queryObject->getConnectionName();
+        }
+
+        return $this->queryExecutorFactory->createWithConnection($connection);
+    }
+}
