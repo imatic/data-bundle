f7aed41c47d6e3bb02cbd1f808a65e43b0c2b4ff
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2015-04-28 18:13:31 +0200

    RESEARCH-124: úprava filtru - rozlišování velkých a malých písmen a použití trim

---
+++ Doctrine/Postgresql/Query/AST/ILike.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Imatic\Bundle\DataBundle\Doctrine\Postgresql\Query\AST;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\Lexer;
+
+class ILike extends FunctionNode
+{
+    protected $field;
+    protected $value;
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return $this->field->dispatch($sqlWalker) . ' ILIKE ' . $this->value->dispatch($sqlWalker);
+    }
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->field = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_COMMA);
+
+        $this->value = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
