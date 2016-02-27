9b606007a89296c754810fba8b91b1ca6bdd3b4f
Stepan Koci <stepan.koci@imatic.cz>
2016-02-27 23:47:00 +0100

    Support for test db configuration and Symfony 3.

--- Tests/Fixtures/TestProject/TestKernel.php
+++ Tests/Fixtures/TestProject/TestKernel.php
@@ -6,6 +6,13 @@ use Imatic\Bundle\TestingBundle\Test\TestKernel as BaseTestKernel;
 
 class TestKernel extends BaseTestKernel
 {
+    public function __construct()
+    {
+        $this->loadConfigFromPhpUnit();
+
+        parent::__construct();
+    }
+
     public function registerBundles()
     {
         $parentBundles = parent::registerBundles();
@@ -18,5 +25,24 @@ class TestKernel extends BaseTestKernel
         ];
 
         return array_merge($parentBundles, $bundles);
+    }
+
+    /**
+     * Load config from phpunit.xml if no phpunit context exists.
+     */
+    public function loadConfigFromPhpUnit()
+    {
+        $files = ['phpunit.xml', 'phpunit.xml.dist'];
+        array_map(function ($file) {
+            $file = __DIR__ . '/../../../' . $file;
+            if (file_exists($file)) {
+                $element = simplexml_load_file($file);
+                foreach ($element->xpath('/phpunit/php/const') as $const) {
+                    if (!defined($const['name'])) {
+                        define($const['name'], $const['value']);
+                    }
+                }
+            }
+        }, $files);
     }
 }
