0dd7c6e3ab16aa4448c0a3402abc9ca990b40e5b
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2017-04-04 11:34:42 +0200

    STILLAP-215: Testování modulu dokumenty
    - fixnutí nastavení správného času přes ruleValue metodu

--- Tests/Unit/Data/Query/DisplayCriteria/Filter/DateRangeRuleTest.php
+++ Tests/Unit/Data/Query/DisplayCriteria/Filter/DateRangeRuleTest.php
@@ -9,10 +9,25 @@ use Imatic\Bundle\DataBundle\Data\Query\DisplayCriteria\Filter\DateRangeRule;
  */
 class DateRangeRuleTest extends \PHPUnit_Framework_TestCase
 {
-    public function testItTimeShouldBeUpdatedSoStartIsAtStartOfTheDayAndEndIsAtEndOfTheDay()
+    public function testTimeShouldBeUpdatedSoStartIsAtStartOfTheDayAndEndIsAtEndOfTheDay()
     {
         $dateRangeRule = new DateRangeRule('name');
         $dateRangeRule->setValue([
+            'start' => new \DateTime('2000-01-01 10:15:32'),
+            'end' => new \DateTime('2000-01-05'),
+        ]);
+
+        $start = new \DateTime('2000-01-01 00:00:00');
+        $end = new \DateTime('2000-01-05 23:59:59');
+        $this->assertTrue($dateRangeRule->isBound());
+        $this->assertEquals($start->getTimestamp(), $dateRangeRule->getValue()['start']->getTimestamp());
+        $this->assertEquals($end->getTimestamp(), $dateRangeRule->getValue()['end']->getTimestamp());
+    }
+
+    public function testTimeShouldBeUpdatedSoStartIsAtStartOfTheDayAndEndIsAtEndOfTheDayIfRuleValueIsUsed()
+    {
+        $dateRangeRule = new DateRangeRule('name');
+        $dateRangeRule->ruleValue([
             'start' => new \DateTime('2000-01-01 10:15:32'),
             'end' => new \DateTime('2000-01-05'),
         ]);
