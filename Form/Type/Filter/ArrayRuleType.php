006b9b823028aa191a922e078f446f393f99958a
Lukas Landa <landa.lukas@gmail.com>
2019-03-09 12:51:09 +0100

    Filter arrays out of scalar form types
    - https://github.com/symfony/symfony/pull/29307

--- Form/Type/Filter/ArrayRuleType.php
+++ Form/Type/Filter/ArrayRuleType.php
@@ -5,12 +5,20 @@ use Imatic\Bundle\DataBundle\Form\DataTransformer\ArrayToStringTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArrayRuleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->addModelTransformer(new ArrayToStringTransformer());
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'multiple' => true,
+        ]);
     }
 
     public function getParent()
